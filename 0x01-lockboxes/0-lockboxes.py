fab8ec8247d346aae5757b14cc00a069482929ef
wathika-eng <wathika02@gmail.com>
2024-10-11 00:06:31 +0300

    fix format

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,13 +15,16 @@ The first box boxes[0] is unlocked
 Return True if all boxes can be opened, else return False
 """
 
+
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
     if not boxes:
         return False
+
     keys = [0]
     for key in keys:
         for box in boxes[key]:
             if box not in keys and box < len(boxes):
                 keys.append(box)
+
     return len(keys) == len(boxes)
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-canUnlockAll = __import__('0-lockboxes').canUnlockAll
+canUnlockAll = __import__("0-lockboxes").canUnlockAll
 
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))
